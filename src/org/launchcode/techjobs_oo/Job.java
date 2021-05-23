454d6a4a25cdb1d3b783ed8320d790f69f89c53a
admin <admin>
2021-05-23 12:58:21 -0500

    Just gotta DRY off

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -20,6 +20,7 @@ public class Job {
     }
 
     public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+        this();
         this.name = name;
         this.employer = employer;
         this.location = location;
@@ -83,5 +84,40 @@ public class Job {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        if (this.employer.getValue().equals("") && this.name.equals("")
+                && this.location.getValue().equals("") && this.positionType.getValue().equals("")
+                && this.coreCompetency.getValue().equals("")) {
+            return "OOPS! This job does not seem to exist.";
+        }
+        String nameValue = this.name;
+        String employerValue = this.getEmployer().toString();
+        String locationValue = this.getLocation().toString();
+        String positionTypeValue = this.getPositionType().toString();
+        String coreCompetencyValue = this.getCoreCompetency().toString();
+
+        if (this.employer.getValue().equals("")) {
+            employerValue = "Data not available";
+        }
+        if (this.name.equals("")) {
+            nameValue = "Data not available";
+        }
+        if (this.location.getValue().equals("")) {
+            locationValue = "Data not available";
+        }
+        if (this.positionType.getValue().equals("")) {
+            positionTypeValue = "Data not available";
+        }
+        if (this.coreCompetency.getValue().equals("")) {
+            coreCompetencyValue = "Data not available";
+        }
+
+        return "\nID: " + this.id+ "\nName: " + nameValue + "\nEmployer: "
+                + employerValue + "\nLocation: " + locationValue
+                + "\nPosition Type: " + positionTypeValue
+                + "\nCore Competency: " + coreCompetencyValue + "\n";
     }
 }
